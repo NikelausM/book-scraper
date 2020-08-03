0622a807e969ead18bc64f27cd0691493cabbd18
NikelausM <jnickm@gmail.com>
2020-08-03 17:02:13 -0600

    Add locator and page classes

---
+++ locators/book_locators.py
@@ -0,0 +1,6 @@
+class BookLocators:
+    """Class containing the CSS locators for books."""
+    NAME_LOCATOR = "article.product_pod h3 a"
+    LINK_LOCATOR = "article.product_pod h3 a"
+    PRICE_LOCATOR = "article.product_pod p.price_color"
+    RATING_LOCATOR = "article.product_pod p.star-rating"
---
+++ pages/all_books_page.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+from locators.all_books_page import AllBooksPageLocators
+from typing import List
+
+class AllBooksPage:
+    def __init__(self, page_content: bytes):
+        """
+        Parameters
+        ----------
+        page_content : bytes
+            The content of the webpage.
+        """
+        self.soup = BeautifulSoup(page_content, "html.parser")
+    
+    @property
+    def books(self) -> List(BookParser):
+        """Returns the books on the books index page."""
+        return [BookParser(e) for e in self.soup.select(AllBooksPageLocators.BOOKS)]
+
+    
