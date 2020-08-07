6e17cd88350be31b786620fc9de581fc1707ce70
NikelausM <jnickm@gmail.com>
2020-08-07 12:20:22 -0600

    Add some async code and imports

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-import requests
 import logging
+import asyncio
+import aiohttp
+import time
+
+import menu
+
 from progress.bar import Bar
 
 from pages.all_books_page import AllBooksPage
 
-import menu
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    # if session.get takes longer than 10 seconds then
+    # an exception will be raised
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            # before returning response status it can get suspended
+            # until response is received
+            print(f"Page took {time.time() - page_start}")
+            return response.status
+
+
+async def get_multiple_pages(loop, *urls):
+    tasks = []
+    # use same loop as before
+    async with aiohttp.ClientSession(loop=loop) as session:
+        for url in urls:
+            # Create coroutine and put in tasks list
+            tasks.append(fetch_page(session, url))
+        # wait for tasks here
+        # gather alls tasks in list and treat as single task
+        # to execute in loop. Awaits each task and only returns
+        # when all complete
+        grouped_tasks = asyncio.gather(*tasks)
+
+        # we need to await them (yield from)
+        # allows us to suspend execution here and wait
+        # until something happens then resume
+        return await grouped_tasks
 
 if __name__ == "__main__":
     logging.basicConfig(
